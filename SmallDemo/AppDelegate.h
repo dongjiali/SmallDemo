2908ec215046ae484d20745d15cdb2555d544ea4
Dong JiaLi <Dongjiali@localhost.(none)>
2012-07-09 11:21:12 +0800

    Initial Commit

---
+++ SmallDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SmallDemo
+//
+//  Created by JiaLi Dong on 12-7-9.
+//  Copyright (c) 2012年 sinsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SmallDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SmallDemo
+//
+//  Created by JiaLi Dong on 12-7-9.
+//  Copyright (c) 2012年 sinsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
